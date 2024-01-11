d2f1f27ae554d1f6dbb67684ac48b5133f50a3f7
saminoorsyed <saminoorsyed@gmail.com>
2024-01-11 10:48:40 -0500

    Section 1.3: Basic Parsing of Tokens
    Description:
    - wrote a lexer which advances the read pointer
    - takes into account multiple chars and #s that may be strung together
    - expanded Tokens
    - tested a longer string of legal chars

---
+++ wig/monkey/lexer/lexer.go
@@ -0,0 +1,112 @@
+package lexer
+
+import "monkey/token"
+
+// struct to facilitate reading input
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position (after current char)
+	ch           byte //current char being examined
+}
+
+// set struct to input
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// helper Method on Lexer struct gives us the next char and advances readPosition
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		//if we're at the end of the input
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+// This Second helper method on the Lexer struct gives the next token
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok // early exit bc readIdentifier has already advanced l.position
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+	l.readChar()
+	return tok
+}
+
+// same as readIdentifier, but for digits
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// I wonder how hexidecimal would work here
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+// reads in an identifier & advances lexer’s positions until a non-letter-character.
+func (l *Lexer) readIdentifier() string {
+	// keep track of the first char position
+	position := l.position
+	// loop til isLetter returns false
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+// can add extra chars here like ? and !
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
